904b84c5ac2d5e82efc6221ac7781848678f3b5a
Saverio Casabianca <Saveriocoppola97@virgilio.it>
2026-05-15 17:17:10 +0200

    Update Other.jsx

--- src/components/Other.jsx
+++ src/components/Other.jsx
@@ -3,7 +3,7 @@ import { Row, Col, Button } from "react-bootstrap";
 const GridExample = function () {
   return (
     <>
-      <Row xs={1} lg={3} className="g-3 my-3">
+      <Row xs={1} lg={3} className="g-1 my-3">
         <Col>
           <Button
             style={{ fontSize: "0.7rem" }}
@@ -32,7 +32,7 @@ const GridExample = function () {
           </Button>
         </Col>
       </Row>
-      <Row xs={1} lg={3} className=" my-3">
+      <Row xs={1} lg={3} className="g-1 my-3">
         <Col>
           <Button
             style={{ fontSize: "0.7rem" }}
@@ -61,7 +61,7 @@ const GridExample = function () {
           </Button>
         </Col>
       </Row>
-      <Row xs={1} lg={3} className=" my-3">
+      <Row xs={1} lg={3} className="g-1 my-3">
         <Col>
           <Button
             style={{ fontSize: "0.7rem" }}
